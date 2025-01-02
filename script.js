f2b7660296ebec5491a95dc086918856af1a7c7f
muazsazelim <muazsazelim019@gmail.com>
2025-01-02 16:27:57 +0000

    project done without extra credit

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
 
 const topContainer = document.querySelector("#top-container");
 
+const buttonContainer = document.createElement('div');
+buttonContainer.id = 'button-container';
+topContainer.appendChild(buttonContainer);
+
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('grid-container');
 topContainer.appendChild(gridContainer);
 
 let gridSize = 16;
 
-for (let i = 0; i < gridSize; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    for (let i = 0; i < gridSize; i++) {
-        const squareSize = 960 / gridSize;
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
-        grid.appendChild(square);
-    }
-    gridContainer.appendChild(grid)
+
+function changeColor(square) {
+    square.style.backgroundColor = '#535461';
 }
+
+function makeGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        for (let i = 0; i < gridSize; i++) {
+            const squareSize = 800 / gridSize;
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            square.addEventListener('mouseover', function() {
+                changeColor(square);
+            });
+            grid.appendChild(square);
+        }
+        gridContainer.appendChild(grid)
+    }
+}
+
+function makeButtons() {
+    const gridButton = document.createElement('button');
+    gridButton.innerText = 'Change Size';
+    gridButton.addEventListener('click', function() {
+        gridSize = parseInt(prompt('Enter a number 1 - 100'));
+
+        if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+            alert('Enter number 1 - 100 only.');
+        } else {
+            while (gridContainer.firstChild) {
+                gridContainer.removeChild(gridContainer.firstChild);
+            }
+    
+            makeGrid();
+        }
+        
+    });
+    buttonContainer.appendChild(gridButton);
+}
+
+// Main Function
+
+makeButtons()
+makeGrid()
